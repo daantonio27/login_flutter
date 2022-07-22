e68ea17042bd5dff2ca580c9118c135872b1e8bc
Da Antonio <daantonioc3@gmail.com>
2022-07-22 04:22:33 +0000

    first commit

---
+++ lib/signup_screen.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => InitState();
+}
+
+class InitState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) => initWidget();
+
+  Widget initWidget() {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90)),
+                    color: new Color(0xffF5591F),
+                    gradient: LinearGradient(colors: [(new  Color(0xffF5591F)), new Color(0xffF2861E)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 50),
+                            child: Image.asset(
+                              "images/logo.png",
+                              height: 90,
+                              width: 90,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 20, top: 20),
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              "Inscrivez-vous",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 50),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xffF5591F),
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.person,
+                        color: Color(0xffF5591F),
+                      ),
+                      hintText: "Nom complet",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xffF5591F),
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.email,
+                        color: Color(0xffF5591F),
+                      ),
+                      hintText: "E-mail",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0xffEEEEEE),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 20),
+                          blurRadius: 100,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xffF5591F),
+                    decoration: InputDecoration(
+                      focusColor: Color(0xffF5591F),
+                      icon: Icon(
+                        Icons.phone,
+                        color: Color(0xffF5591F),
+                      ),
+                      hintText: "Numéro de mobile",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0xffEEEEEE),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 20),
+                          blurRadius: 100,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xffF5591F),
+                    decoration: InputDecoration(
+                      focusColor: Color(0xffF5591F),
+                      icon: Icon(
+                        Icons.vpn_key,
+                        color: Color(0xffF5591F),
+                      ),
+                      hintText: "Mot de passe",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () {
+                    // Write Click Listener Code Here.
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 20, right: 20, top: 50),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    height: 54,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [(new  Color(0xffF5591F)), new Color(0xffF2861E)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.grey[200],
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 50,
+                            color: Color(0xffEEEEEE)
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      "S'inscrire",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Vous avez un compte ? "),
+                      GestureDetector(
+                        child: Text(
+                          "Connectez-vous",
+                          style: TextStyle(
+                              color: Color(0xffF5591F)
+                          ),
+                        ),
+                        onTap: () {
+                          // Write Tap Code Here.
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+        )
+    );
+  }
+}
